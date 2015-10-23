9f5192a9e3bbd0097f688b34a796b0bf72fe07bd
Benjamin Barnes <benjaminrbarnes@gmail.com>
2015-10-22 17:42:53 -0700

    Client now sends login to server

--- client.cpp
+++ client.cpp
@@ -1,38 +1,27 @@
 #include "client.h"
 #include "duckchat.h"
 
-//#include <stdio.h>
-//#include <sys/socket.h>
-//#include <string.h>
-//#include <netinet/in.h>
-//#include <stdlib.h>
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <sstream>
 #include <netinet/in.h>
 
-//using namespace std;
-
-//enum Commands {EXIT, JOIN, LEAVE, LIST, WHO, SWITCH};
 int kClientPort = 5001;
 struct sockaddr_in client_addr;
 struct sockaddr_in server_addr;
 int client_socket;
 std::vector<char *> delete_queue;
 
+
+
+
 void Error(const char *msg) {
   perror(msg);
   exit(1);
 }
 
-//char *StringToCharStar(std::string input) {
-//  char *tmp = new char[input.length() + 1];
-//  strcpy(tmp, input.c_str());
-//
-//  return tmp;
-//}
+
 
 void Connect(char *server, int port) {
   printf("Connecting to %s\n", server);
@@ -56,16 +45,23 @@ void Connect(char *server, int port) {
   }
 }
 
-void Send(std::string input) {
-  char *message = new char[input.length() + 1];
-  strcpy(message, input.c_str());
 
-  if (sendto(client_socket, message, strlen(message), 0, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+
+
+int SendLogin(struct request_login user_login) {
+  void* message[sizeof(struct request_login)];
+  memcpy(message, &user_login, sizeof(struct request_login));
+
+  if (sendto(client_socket, message, sizeof(struct request_login), 0, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
     Error("client: failed to send message\n");
+    return 1;
   }
 
-  delete message;
+  return 0;
 }
+
+
+
 
 std::vector<std::string> StringSplit(std::string input) {
   std::istringstream iss(input);
@@ -74,12 +70,15 @@ std::vector<std::string> StringSplit(std::string input) {
   return result;
 }
 
+
+
+
 bool ProcessInput(std::string input) {
   std::vector<std::string> inputs = StringSplit(input);
   bool result = true;
 
   if (inputs[0] == "/exit") {
-    Send(inputs[0]);
+//    Send(inputs[0]);
     result = false;
   } else if (inputs[0] == "/list") {
 
@@ -98,12 +97,15 @@ bool ProcessInput(std::string input) {
   return result;
 }
 
+
+
+
+
 int main(int argc, char *argv[]) {
   char *server;
   int port;
-  std::string username;
+  char *username;
   std::string input;
-//  delete_queue = std::vector<char *>();
 
   if (argc < 4) {
     fprintf(stderr,"usage: client [server name] [port] [username]\n");
@@ -116,7 +118,12 @@ int main(int argc, char *argv[]) {
 
   Connect(server, port);
 
-  Send(username);
+  struct request_login user_login;
+  memset((char *) &user_login, 0, sizeof(user_login));
+  user_login.req_type = REQ_LOGIN;
+  strncpy(user_login.req_username, username, USERNAME_MAX);
+
+  SendLogin(user_login);
 
   // TODO handle response from send
 
@@ -130,7 +137,7 @@ int main(int argc, char *argv[]) {
       }
     } else {
       // Sending chat messages
-      Send(input);
+//      Send(input);
     }
 
   }
