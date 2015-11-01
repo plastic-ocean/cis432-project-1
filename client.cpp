2bc8a7d5237d921f05a6a7852d3521de8e5bc415
Keith Hamm <hkhamm@gmail.com>
2015-11-01 12:34:23 -0800

    create arbitrary size of user_infos in tmp, print first user client

--- client.cpp
+++ client.cpp
@@ -292,40 +292,40 @@ void HandleError(char *receive_buffer, char *output) {
 
 void HandleTextWho(char *receive_buffer, char *output) {
   struct text_who tmp;
-  memcpy(&tmp, receive_buffer, sizeof(struct text_who));
+  memcpy(&tmp, receive_buffer, sizeof(struct text_who) + (10 * sizeof(user_info)));
 
-  std::cout << "before malloc" << std::endl;
+  std::cout << "first user in tmp" << tmp.txt_users->us_username[0] << std::endl;
 
-  int user_info_size = tmp.txt_nusernames;
-  size_t who_size = sizeof(text_who) + (user_info_size * sizeof(user_info));
-  struct text_who *who = (text_who *) malloc(who_size);
-
-  std::cout << "after malloc" << std::endl;
-
-  memset(who, '\0', who_size);
-  memcpy(&who, receive_buffer, who_size);
-
-  std::cout << "after memcpy" << std::endl;
-
+//  int user_info_size = tmp.txt_nusernames;
+//  size_t who_size = sizeof(text_who) + (user_info_size * sizeof(user_info));
+//  struct text_who *who = (text_who *) malloc(who_size);
+//  memset(who, '\0', who_size);
+//
+//  std::cout << "after malloc" << std::endl;
+//
+//  memcpy(&who, receive_buffer, who_size);
+//
+//  std::cout << "after memcpy" << std::endl;
+//
   std::string backspaces = "";
   for (int i = 0; i < SAY_MAX; i++) {
     backspaces.append("\b");
   }
   std::cout << backspaces;
+//
+//  std::cout << "after backspaces" << std::endl;
+//
+//  std::cout << "Users on channel " << who->txt_channel << ":" << std::endl;
+//  std::cout << "number of people " << who->txt_nusernames << std::endl;
 
-  std::cout << "after backspaces" << std::endl;
-
-  std::cout << "Users on channel " << who->txt_channel << ":" << std::endl;
-  std::cout << "number of people " << who->txt_nusernames << std::endl;
-
-  for (int i = 0; i < who->txt_nusernames + 2; i++) {
-    std::cout << " " << who->txt_users[i].us_username << std::endl;
-  }
+//  for (int i = 0; i < who->txt_nusernames + 2; i++) {
+//    std::cout << " " << who->txt_users[i].us_username << std::endl;
+//  }
 
   PrintPrompt();
   std::cout << output << std::flush;
 
-  free(who);
+//  free(who);
 }
 
 
