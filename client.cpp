455aba251eeaf7cfadfbe5f53c92b8777e022670
Keith Hamm <hkhamm@gmail.com>
2015-10-31 22:22:40 -0700

    test ntohl client

--- client.cpp
+++ client.cpp
@@ -486,7 +486,7 @@ int main(int argc, char *argv[]) {
         if (read_size != 0) {
           struct text message;
           memcpy(&message, receive_buffer, sizeof(struct text));
-          text_t text_type = message.txt_type;
+          text_t text_type = ntohl(message.txt_type);
 
           std::cout << "type: " << text_type << std::endl;
 
