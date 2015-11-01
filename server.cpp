71c8337808ea18b8f7eb47d836d0e493916d21e8
Keith Hamm <hkhamm@gmail.com>
2015-10-31 20:27:53 -0700

    move leave to function

--- server.cpp
+++ server.cpp
@@ -52,17 +52,17 @@ void Error(const char *msg) {
 }
 
 
-void RemoveUser(User *user){
-  for(auto channel : kChannels){
-    for(auto channel_user : channel.second->users){
-      if(channel_user->name == user->name){
+void RemoveUser(User *user) {
+  for(auto channel : kChannels) {
+    for(auto channel_user : channel.second->users) {
+      if (channel_user->name == user->name) {
         channel.second->users.remove(channel_user);
         break;
       }
     }
   }
-  for(auto current_user : kUsers){
-    if(current_user.second->name == user->name){
+  for(auto current_user : kUsers) {
+    if (current_user.second->name == user->name) {
       kUsers.erase(user->name);
     }
   }
@@ -113,13 +113,74 @@ void HandleLogoutRequest(void *buffer, in_addr_t request_address, unsigned short
 
 
 /**
+ * Removes a user from the requested channel.
+ *
+ * @buffer is the logout_request
+ * @request_address is the user's address.
+ * @request_port is the user's port.
+ */
+void HandleLeaveRequest(void *buffer, in_addr_t request_address, unsigned short request_port) {
+  Channel *channel;
+  std::string current_channel;
+  std::list<User *>::const_iterator it;
+  bool is_channel;
+  struct request_leave leave_request;
+  
+  memcpy(&leave_request, buffer, sizeof(struct request_leave));
+  current_channel = leave_request.req_channel;
+
+  for (auto user : kUsers) {
+    unsigned short current_port = user.second->port;
+    in_addr_t current_address = user.second->address;
+
+    if (current_port == request_port && current_address == request_address) {
+      is_channel = false;
+      for(auto ch : kChannels) {
+        if (ch.first == current_channel) {
+          std::cout << "channel found" << std::endl;
+          is_channel = true;
+          break;
+        }
+      }
+
+      if (is_channel) {
+        channel = kChannels[current_channel];
+
+        for (it = channel->users.begin(); it != channel->users.end(); ++it) {
+          if ((*it)->name == user.first) {
+            break;
+          }
+        }
+
+        if ( it != channel->users.end()) {
+          channel->users.remove(*it);
+          std::cout << user.first << " leaves channel " << channel->name << std::endl;
+          if (channel->users.size() == 0) {
+            kChannels.erase(channel->name);
+            std::cout << "server: removing empty channel " << channel->name << std::endl;
+          }
+        }
+        for (auto u : channel->users) {
+          std::cout << "user: " << u->name << std::endl;
+        }
+        break;
+      } else {
+        std::cout << "server: " << user.first << " trying to leave non-existent channel " << channel->name << std::endl;
+      }
+
+    }
+  }
+}
+
+
+/**
  * Sends a text list packet containing every channel to the requesting user.
  *
  * @server_socket is the socket to send on.
  * @request_address is the address to send to.
  * @request_port is the port to send to.
  */
-void SendTextList(int server_socket, in_addr_t request_address, unsigned short request_port) {
+void HandleListRequest(int server_socket, in_addr_t request_address, unsigned short request_port) {
   struct sockaddr_in client_addr;
   const size_t list_size = sizeof(text_list) + (kChannels.size() * sizeof(channel_info));
   struct text_list *list = (text_list *) malloc(list_size);
@@ -165,7 +226,7 @@ void SendTextList(int server_socket, in_addr_t request_address, unsigned short r
 }
 
 
-struct sockaddr_in* CreateSockAddr(unsigned short port, in_addr_t address){
+struct sockaddr_in* CreateSockAddr(unsigned short port, in_addr_t address) {
   struct sockaddr_in *client_addr;
   memset(&client_addr, 0, sizeof(struct sockaddr_in));
   client_addr->sin_family = AF_INET;
@@ -196,50 +257,7 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
       HandleLogoutRequest(buffer, request_address, request_port);
       break;
     case REQ_LEAVE:
-      struct request_leave leave_request;
-      memcpy(&leave_request, buffer, sizeof(struct request_leave));
-      current_channel = leave_request.req_channel;
-      for (auto user : kUsers) {
-        unsigned short current_port = user.second->port;
-        in_addr_t current_address = user.second->address;
-
-        if (current_port == request_port && current_address == request_address) {
-          is_channel = false;
-          for(auto ch : kChannels){
-            if(ch.first == current_channel){
-              std::cout << "channel found" << std::endl;
-              is_channel = true;
-              break;
-            }
-          }
-
-          if(is_channel){
-            channel = kChannels[current_channel];
-
-            for (it = channel->users.begin(); it != channel->users.end(); ++it){
-              if((*it)->name == user.first){
-                break;
-              }
-            }
-
-            if( it != channel->users.end()){
-              channel->users.remove(*it);
-              std::cout << user.first << " leaves channel " << channel->name << std::endl;
-              if (channel->users.size() == 0) {
-                kChannels.erase(channel->name);
-                std::cout << "server: removing empty channel " << channel->name << std::endl;
-              }
-            }
-            for (auto u : channel->users) {
-              std::cout << "user: " << u->name << std::endl;
-            }
-            break;
-          } else {
-            std::cout << "server: " << user.first << " trying to leave non-existent channel " << channel->name << std::endl;
-          }
-
-        }
-      }
+      HandleLeaveRequest(buffer, request_address, request_port);
       break;
     case REQ_JOIN:
       struct request_join join_request;
@@ -263,22 +281,18 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
       for (auto user : kUsers) {
         unsigned short current_port = user.second->port;
         in_addr_t current_address = user.second->address;
-//        std::cout << std::endl;
-//        std::cout << "port: " << current_port << " address "<< current_address << std::endl;
-//        std::cout << "port: " << request_port << " address "<< request_address << std::endl;
-//        std::cout << std::endl;
         if (current_port == request_port && current_address == request_address) {
           std::cout << "server: " << user.first << " joins channel "<< channel->name << std::endl;
 
           is_channel_user = false;
-          for(auto u : channel->users){
-            if(u->name == user.second->name){
+          for(auto u : channel->users) {
+            if (u->name == user.second->name) {
               is_channel_user = true;
               break;
             }
           }
 
-          if(!is_channel_user){
+          if (!is_channel_user) {
             channel->users.push_back(user.second);
           }
 
@@ -286,11 +300,6 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
           if (is_new_channel) {
             kChannels.insert({channel->name, channel});
           }
-
-          // Test print
-//          for (auto u : channel->users) {
-//            std::cout << "user: " << u->name << std::endl;
-//          }
           break;
         }
 
@@ -306,7 +315,7 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
 
         if (current_port == request_port && current_address == request_address) {
           current_user = user.second;
-          for(auto channel_user : kChannels[say_request.req_channel]->users){
+          for(auto channel_user : kChannels[say_request.req_channel]->users) {
             struct sockaddr_in client_addr;
             struct text_say say;
             memcpy(&say, buffer, sizeof(struct text_say));
@@ -336,75 +345,8 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
 
 
       break;
-
-
     case REQ_LIST:
-      SendTextList(server_socket, request_address, request_port);
-//      struct sockaddr_in client_addr;
-//      const size_t list_size = sizeof(text_list) + (kChannels.size() * sizeof(channel_info));
-//      struct text_list *list = (text_list *) malloc(list_size);
-//      memset(list, '\0', list_size);;
-//
-//      list->txt_type = TXT_LIST;
-//      list->txt_nchannels = (int) kChannels.size();
-//
-//      i = 0;
-//      for (auto ch : kChannels) {
-//        strncpy(list->txt_channels[i++].ch_channel, ch.first, CHANNEL_MAX);
-//        std::cout << "channel name: " << list->txt_channels[i].ch_channel << std::endl;
-//      }
-
-//      channel_list = new channel_info[list.txt_nchannels];
-//      i = 0;
-//      for(auto ch : kChannels){
-//        std::cout << "at beginning "<< std::endl;
-//        struct channel_info new_info;
-//        strncpy(new_info.ch_channel, ch.second->name.c_str(), CHANNEL_MAX);
-//        memcpy(&channel_list[i], &new_info, sizeof(struct channel_info));
-////        memcpy(&list.txt_channels[i], &channel_list[i], sizeof(struct channel_info));
-////        std::cout << "channel name: " << list.txt_channels[i].ch_channel << std::endl;
-//        i++;
-//      }
-//
-//      memcpy(&list.txt_channels, channel_list, sizeof(*channel_list));
-//      std::cout << "at end "<< std::endl;
-//      for(i = 0; i < list.txt_nchannels; i++){
-//        std::cout << "channel name: " << list.txt_channels[i].ch_channel << std::endl;
-//      }
-//      std::cout << "size of array: " << sizeof(list.txt_channels) / sizeof(struct channel_info) << std::endl;
-
-//      std::cout << "length of list nchannels : " << list.txt_nchannels << std::endl;
-//      memset(list.txt_channels, 0, list.txt_nchannels * sizeof(struct channel_info));
-//      i = 0;
-//      for(auto ch : kChannels){
-//        struct channel_info new_channel;
-//        memset(new_channel.ch_channel, 0, CHANNEL_MAX);
-//        strncpy(new_channel.ch_channel, ch.second->name.c_str(), CHANNEL_MAX);
-//        std::cout << "channel: " << new_channel.ch_channel << std::endl;
-////        std::cout << "size of array: " << sizeof(list.txt_channels) / sizeof(struct channel_info) << std::endl;
-////        list.txt_channels[i++] = new_channel;
-//        memcpy(&list.txt_channels[i++], &new_channel, sizeof(new_channel));
-//      }
-//      for (auto user : kUsers) {
-//        std::cout << "user loop" << std::endl;
-//        unsigned short current_port = user.second->port;
-//        in_addr_t current_address = user.second->address;
-//        if (current_port == request_port && current_address == request_address) {
-//          std::cout << "server: " << user.first << " lists channels" << std::endl;
-//          current_user = user.second;
-//          memset(&client_addr, 0, sizeof(struct sockaddr_in));
-//          client_addr.sin_family = AF_INET;
-//          client_addr.sin_port = current_user->port;
-//          client_addr.sin_addr.s_addr = current_user->address;
-//          size_t message_size = sizeof(struct text_list);
-//
-//          if (sendto(server_socket, &list, message_size, 0, (struct sockaddr*) &client_addr, sizeof(client_addr)) < 0) {
-//            Error("server: failed to send say\n");
-//          }
-//          break;
-//        }
-//      }
-
+      HandleListRequest(server_socket, request_address, request_port);
       break;
     default:
       break;
