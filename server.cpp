d0730aa75b2419f1a55f3882c2ab0590726431d9
Benjamin Barnes <benjaminrbarnes@gmail.com>
2015-10-31 17:49:02 -0700

    size fix

--- server.cpp
+++ server.cpp
@@ -278,7 +278,7 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
         memset(new_channel.ch_channel, 0, CHANNEL_MAX);
         strncpy(new_channel.ch_channel, ch.second->name.c_str(), CHANNEL_MAX);
         std::cout << "channel: " << new_channel.ch_channel << std::endl;
-        std::cout << "size of array: " << sizeof(struct channel_info) / sizeof(channel_info[0]) << std::endl;
+        std::cout << "size of array: " << sizeof(list.txt_channels) / sizeof(channel_info) << std::endl;
 //        list.txt_channels[i++] = new_channel;
         memcpy(&list.txt_channels[i++], &new_channel, sizeof(new_channel));
       }
