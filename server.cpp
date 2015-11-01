b615784ddeb3b951c13614598b86dacb8802c306
Benjamin Barnes <benjaminrbarnes@gmail.com>
2015-10-31 18:43:39 -0700

    reference removed

--- server.cpp
+++ server.cpp
@@ -281,11 +281,11 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
         struct channel_info new_info;
         strncpy(new_info.ch_channel, ch.second->name.c_str(), CHANNEL_MAX);
         memcpy(&channel_list[i], &new_info, sizeof(struct channel_info));
-        memcpy(&list.txt_channels[i].ch_channel, &channel_list[i].ch_channel, CHANNEL_MAX);
         std::cout << "channel name: " << list.txt_channels[i].ch_channel << std::endl;
-//        i++;
+        memcpy(&list.txt_channels[i].ch_channel, channel_list[i].ch_channel, CHANNEL_MAX);
+        i++;
       }
-          std::cout << "at end "<< std::endl;
+      std::cout << "at end "<< std::endl;
 
 //      std::cout << "size of array: " << sizeof(list.txt_channels) / sizeof(struct channel_info) << std::endl;
 
