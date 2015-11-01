405d8b34afab74a9650f4423d77cecbce284e0d9
Benjamin Barnes <benjaminrbarnes@gmail.com>
2015-10-31 18:55:37 -0700

    memcpy on iteration

--- server.cpp
+++ server.cpp
@@ -281,16 +281,16 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
         struct channel_info new_info;
         strncpy(new_info.ch_channel, ch.second->name.c_str(), CHANNEL_MAX);
         memcpy(&channel_list[i], &new_info, sizeof(struct channel_info));
-//        memcpy(&list.txt_channels[i].ch_channel, &channel_list[i].ch_channel, CHANNEL_MAX);
-//        std::cout << "channel name: " << list.txt_channels[i].ch_channel << std::endl;
+        memcpy(&list.txt_channels[i], &channel_list[i], sizeof(struct channel_info));
+        std::cout << "channel name: " << list.txt_channels[i].ch_channel << std::endl;
         i++;
       }
-        memcpy(&list.txt_channels, channel_list, sizeof(*channel_list));
+//        memcpy(&list.txt_channels, channel_list, sizeof(*channel_list));
 //      std::cout << "at end "<< std::endl;
 //          std::cout << "channel name: " << list.txt_channels[0].ch_channel << std::endl;
-          for(i = 0; i < list.txt_nchannels; i++){
-            std::cout << "channel name: " << channel_list[i].ch_channel<< std::endl;
-          }
+//          for(i = 0; i < list.txt_nchannels; i++){
+//            std::cout << "channel name: " << channel_list[i].ch_channel<< std::endl;
+//          }
 
 //      std::cout << "size of array: " << sizeof(list.txt_channels) / sizeof(struct channel_info) << std::endl;
 
