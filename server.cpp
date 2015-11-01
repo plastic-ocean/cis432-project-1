b0b393f2418aa455f5043d749c50b0f2bf28ee93
Benjamin Barnes <benjaminrbarnes@gmail.com>
2015-10-31 18:59:44 -0700

    getting val

--- server.cpp
+++ server.cpp
@@ -289,7 +289,7 @@ void ProcessRequest(int server_socket, void *buffer, in_addr_t request_address, 
       memcpy(&list.txt_channels, channel_list, sizeof(*channel_list));
       std::cout << "at end "<< std::endl;
       for(i = 0; i < list.txt_nchannels; i++){
-        std::cout << "channel name: " << list.txt_channels[i] << std::endl;
+        std::cout << "channel name: " << list.txt_channels[i].ch_channel << std::endl;
       }
 
 //      std::cout << "size of array: " << sizeof(list.txt_channels) / sizeof(struct channel_info) << std::endl;
