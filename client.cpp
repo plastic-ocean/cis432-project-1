028d8e96f8deaab9cf2fa1bc568fc6de56b183b8
Keith Hamm <hkhamm@gmail.com>
2015-11-03 08:41:17 -0800

    who users print statement before users

--- client.cpp
+++ client.cpp
@@ -345,10 +345,9 @@ void HandleTextWho(char *receive_buffer, char *output) {
   }
   std::cout << backspaces;
 
-  std::cout << "Channel: " << who->txt_channel << std::endl;
-  std::cout << "User count: " << who->txt_nusernames << std::endl;
+  std::cout << "Users on channel " << who->txt_channel << ":" << std::endl;
 
-  for (int i = 0; i < who->txt_nusernames + 10; i++) {
+  for (int i = 0; i < who->txt_nusernames; i++) {
     std::cout << " " << who->txt_users[i].us_username << std::endl;
   }
 
