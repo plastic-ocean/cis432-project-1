a92e1e0e787f188060f01eef5c99fa82587a4aad
Keith Hamm <hkhamm@gmail.com>
2015-11-29 10:33:14 -0800

    modify start script

--- start_servers.sh
+++ start_servers.sh
@@ -17,19 +17,19 @@ $SERVER localhost 6000 localhost 6001 &
 $SERVER localhost 6001 localhost 6000 localhost 6002 localhost 6003 &
 $SERVER localhost 6002 localhost 6001 &
 $SERVER localhost 6003 localhost 6001 localhost 6005 &
-$SERVER localhost 6006 localhost 6005 &
-$SERVER localhost 6005 localhost 6006 localhost 6003 localhost 6006 &
+$SERVER localhost 6004 localhost 6005 &
+$SERVER localhost 6005 localhost 6004 localhost 6003 localhost 6006 &
 $SERVER localhost 6006 localhost 6005 &
 
 # Generate a 3x3 grid topology
 #$SERVER localhost 6000 localhost 6001 localhost 6003 &
-#$SERVER localhost 6001 localhost 6000 localhost 6002 localhost 6006 &
+#$SERVER localhost 6001 localhost 6000 localhost 6002 localhost 6004 &
 #$SERVER localhost 6002 localhost 6001 localhost 6005 &
-#$SERVER localhost 6003 localhost 6000 localhost 6006 localhost 6006 &
-#$SERVER localhost 6006 localhost 6001 localhost 6003 localhost 6005 localhost 6007 &
-#$SERVER localhost 6005 localhost 6002 localhost 6006 localhost 6008 &
+#$SERVER localhost 6003 localhost 6000 localhost 6004 localhost 6006 &
+#$SERVER localhost 6004 localhost 6001 localhost 6003 localhost 6005 localhost 6007 &
+#$SERVER localhost 6005 localhost 6002 localhost 6004 localhost 6008 &
 #$SERVER localhost 6006 localhost 6003 localhost 6007 &
-#$SERVER localhost 6007 localhost 6006 localhost 6006 localhost 6008 &
+#$SERVER localhost 6007 localhost 6006 localhost 6004 localhost 6008 &
 #$SERVER localhost 6008 localhost 6005 localhost 6007 &
 
 
